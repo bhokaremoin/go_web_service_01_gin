66128ebb408af026854fe92c69c7793706d85026
ZamZamBakers <75439850+bhokaremoin@users.noreply.github.com>
2023-12-16 22:42:15 +0530

    added the get album by id and add a new album with post

--- main.go
+++ main.go
@@ -22,9 +22,28 @@ var albums = []album{
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
-
+func getAlbumById(c *gin.Context) {
+	id := c.Param("id")
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not " + id + " found"})
+}
+func postAlbum(c *gin.Context) {
+	var newAlbum album
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
 func main() {
 	router := gin.Default()
 	router.GET("/album", getAlbums)
+	router.GET("/album/:id", getAlbumById)
+	router.POST("/album", postAlbum)
 	router.Run("localhost:8080")
 }
